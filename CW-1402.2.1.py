40114c276eacf9b7b26dc37ce3f4926ff8f58480
Abbas moradi <en.moradi.66@gmail.com>
2023-04-21 12:19:09 +0430

    database manager

---
+++ CW-1402.2.1.py
@@ -0,0 +1,83 @@
+import psycopg2
+from psycopg2 import pool
+
+
+class DatabaseManager:
+    def __init__(self, dbname, user, password, host='localhost', port=5432, pool_size=10):
+        self.dbname = dbname
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+        self.pool = pool.SimpleConnectionPool(
+            minconn=1, maxconn=pool_size,
+            dbname=self.dbname, user=self.user,
+            password=self.password, host=self.host,
+            port=self.port
+        )
+
+    def get_connection(self):
+        try:
+            return self.pool.getconn()
+        except psycopg2.Error as e:
+            print("Error connecting to database:", e)
+            raise
+
+    def close_connection(self, conn):
+        try:
+            self.pool.putconn(conn)
+        except psycopg2.Error as e:
+            print("Error closing database connection:", e)
+            raise
+
+    def execute_query(self, query, params=None, fetch=True):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(query, params)
+            if fetch:
+                result = cursor.fetchall()
+                print(*result, sep='\n')
+            else:
+                result = None
+            conn.commit()
+        except Exception as e:
+            conn.rollback()
+            print("Error executing query:", e)
+            raise
+        finally:
+            cursor.close()
+            self.close_connection(conn)
+        return result
+
+    def begin_transaction(self):
+        conn = self.get_connection()
+        conn.autocommit = False
+        return conn
+
+    def commit_transaction(self, conn):
+        try:
+            conn.commit()
+        except psycopg2.Error as e:
+            conn.rollback()
+            print("Error committing transaction:", e)
+            raise
+        finally:
+            self.close_connection(conn)
+
+    def rollback_transaction(self, conn):
+        try:
+            conn.rollback()
+        except psycopg2.Error as e:
+            print("Error rolling back transaction:", e)
+            raise
+        finally:
+            self.close_connection(conn)
+
+
+# customer = DatabaseManager('finance', 'postgres', '@bb@s1366')
+# customer.get_connection()
+# customer.execute_query('''INSERT INTO finance(type, date, amount, category, description)
+#                         VALUES (%s,%s,%s,%s,%s)''', ("income", "2023-04-21", 1000000, "sale", "phone"), False)
+
+# customer.execute_query('''DELETE FROM finance WHERE amount = 950000''', fetch=False)
